4053e72946b9ef2700014936a8b93a3b42466855
Sjoerd Visscher <sjoerd@w3future.com>
2010-12-18 16:21:26 +0100

    Generalize solver to n dimensions.

--- Fling.hs
+++ Fling.hs
@@ -4,41 +4,60 @@
 
 module Fling where
 
-import Control.Arrow (second, (***))
-import Data.List (sort, groupBy, subsequences)
+import Control.Arrow (second, (&&&))
+import Data.List (sort, groupBy)
 import Data.Function (on)
 import Data.Tree (Tree(..), Forest, drawForest)
-import Data.Maybe (fromJust)
 
 
 -- Example puzzles
 
 example0 :: Game
-example0 = [(1,4),(1,5),(2,1),(2,5),(5,5),(7,2)]
+example0 = mkGame [[1,4],[1,5],[2,1],[2,5],[5,5],[7,2]]
 
 example1 :: Game
-example1 = [(0,0),(1,2),(2,0),(3,1),(6,3),(7,1)]
+example1 = mkGame [[0,0],[1,2],[2,0],[3,1],[6,3],[7,1]]
 
 puzzle9_1 :: Game
-puzzle9_1 = [(0,0),(0,2),(1,6),(2,6),(4,1),(5,2),(6,5),(7,3)]
+puzzle9_1 = mkGame [[0,0],[0,2],[1,6],[2,6],[4,1],[5,2],[6,5],[7,3]]
 
 
 -- Some types
 
-type Point = (Y, X)
+type Point = [Int]
 type X     = Int
-type Y     = Int
+type Y     = [Int] -- Dimensions other than X, needs a better name.
+
+newtype Furball = Furball { unFurball :: Point }
+  deriving Eq
 
 type Row = [X]
 
-type Game = [Point]
+type Game = [Furball]
 
-data Move = Move Point Dir
-  deriving (Eq, Show, Read)
+data Move = Move Furball Dir
+  deriving (Eq, Show)
 
-data Dir = North | East | South | West
-  deriving (Eq, Show, Read, Enum, Bounded)
+newtype Dir = Dir { unDir :: Point }
+  deriving (Eq)
 
+instance Show Furball where
+  show (Furball pt) = show pt
+  
+instance Show Dir where
+  show (Dir [ 0, -1]) = "North"
+  show (Dir [ 1,  0]) = "East"
+  show (Dir [ 0,  1]) = "South"
+  show (Dir [-1,  0]) = "West"
+
+  show (Dir [ 1,  0,  0]) = "Right"
+  show (Dir [-1,  0,  0]) = "Left"
+  show (Dir [ 0,  1,  0]) = "Up"
+  show (Dir [ 0, -1,  0]) = "Down"
+  show (Dir [ 0,  0,  1]) = "Forwards"
+  show (Dir [ 0,  0, -1]) = "Backwards"
+  
+  show (Dir vec) = show vec
 
 -- Transforming stuff
 
@@ -47,8 +66,11 @@ type Transformation = [Point -> Point]
 class Transform a where
   transform :: Transformation -> a -> a
 
-instance Transform Point where
-  transform = foldr (.) id
+instance Transform Furball where
+  transform xf = Furball . foldr (.) id xf . unFurball
+
+instance Transform Dir where
+  transform xf = Dir     . foldr (.) id xf . unDir
 
 instance Transform a => Transform [a] where
   transform xf = map (transform xf)
@@ -56,29 +78,26 @@ instance Transform a => Transform [a] where
 instance Transform Move where
   transform xf (Move pt dir) = Move (transform xf pt) (transform xf dir)
 
-instance Transform Dir where
-  transform xf = pointToDir . transform xf . dirToPoint
-    where
-      dirToPoint = fromJust . flip lookup dirPoints
-      pointToDir = fromJust . flip lookup (map swap dirPoints)
-      swap (x, y) = (y, x)
+instance (Transform a, Transform b) => Transform (a, b) where
+  transform xf (a, b) = (transform xf a, transform xf b)
 
-      dirPoints :: [(Dir, Point)]
-      dirPoints =
-        [ (North, (-1,0))
-        , (East,  (0, 1))
-        , (South, (1, 0))
-        , (West,  (0,-1))
-        ]
 
-transformations2D :: [Transformation]
-transformations2D = subsequences [mirrorX, mirrorDiag]
+transformations :: Int -> [(Transformation, Transformation)]
+transformations n = zip
+  [          mx ++ md  | mx <- [[], [mirrorX]], md <- take n $ iterate (   mirrorDiag :) []]
+  [ reverse (mx ++ md) | mx <- [[], [mirrorX]], md <- take n $ iterate (revMirrorDiag :) []]
 
 mirrorX :: Point -> Point
-mirrorX = second negate
+mirrorX [] = []
+mirrorX (x:dims) = -x:dims
 
 mirrorDiag :: Point -> Point
-mirrorDiag (x,y) = (y,x)
+mirrorDiag [] = []
+mirrorDiag (x:dims) = dims ++ [x]
+
+revMirrorDiag :: Point -> Point
+revMirrorDiag [] = []
+revMirrorDiag dims = last dims : init dims
 
 
 -- Top-level API
@@ -104,36 +123,42 @@ solutions = concatMap f
 search :: Game -> Forest (Move, Game)
 search = map (\(m, g') -> Node (m, g') (search g')) . moves
 
+-- | Make a game from a list of coordinates.
+mkGame :: [Point] -> Game
+mkGame = map Furball
 
 -- Generating moves
 
 -- | Noemt gegeven een state alle mogelijke zetten in die state, elk gekoppeld
 -- met de bijbehorende nieuwe state.
 moves :: Game -> [(Move, Game)]
-moves g = concatMap f transformations2D
+moves g = concatMap f (transformations n)
   where
-    f xf = map (transform (reverse xf) *** transform (reverse xf))
+    n    = length . unFurball . head $ g
+    f xf = transform (snd xf)
          . (map . second) fromRows
          . shifts
          . toRows
-         . map (transform xf)
+         . transform (fst xf)
          $ g
 
 groupOn :: Eq b => (a -> b) -> [a] -> [[a]]
 groupOn f = groupBy ((==) `on` f)
 
 toRows :: Game -> [(Y, Row)]
-toRows = map (\row -> (fst (head row), map snd row)) . groupOn fst . sort
+toRows = map (\row -> (fst (head row), map snd row)) . groupOn fst . sort . map ((tail &&& head) . unFurball)
 
 fromRows :: [(Y, Row)] -> Game
-fromRows = concatMap (\(y, row) -> map (y,) row)
+fromRows = concatMap (\(y, row) -> map (Furball . (:y)) row)
 
 -- | Probeert voor alle rijen alle bolletjes naar rechts te rollen.
 shifts :: [(Y, Row)] -> [(Move, [(Y, Row)])]
 shifts [] = []
 shifts ((y, row) : yrows) =
-  map (\(x, r) -> (Move (y, x) East, (y, r) : yrows)) (shift row) ++
+  map (\(x, r) -> (mkMove x y, (y, r) : yrows)) (shift row) ++
   map (second ((y, row) :)) (shifts yrows)
+    where
+      mkMove x dims = Move (Furball $ x : dims) (Dir $ 1 : replicate (length dims) 0)
 
 -- | Probeert voor 1 rij alle balletjes naar rechts te rollen (per balletje shift1).
 shift :: Row -> [(X, Row)]
